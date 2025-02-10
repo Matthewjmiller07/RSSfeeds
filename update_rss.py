e012c98fde03c8ba6c6f59e569b5100b00c3e152
Matthew Miller <Matthewjmiller07@gmail.com>
2025-02-09 22:54:50 -0600

    Update update_rss.py

--- update_rss.py
+++ update_rss.py
@@ -27,7 +27,7 @@ rss_feeds = {
         "description": "Daf Yomi Shiur from Rav Yona Reiss, Shlit”a\nAv Beis Din, Chicago Rabbinical Council (cRc)\nSgan Av Beis Din, Beis Din of America (BDA)\nRosh Yeshiva, RIETS\n\nלע”נ חיים בן סעדיה והב",
         "author": "Rabbi Yona Reiss",  # Added author key
         "cover_art": "https://i.imgur.com/0sOw92Q.jpeg",  # Added cover art URL
-        "email": "info@crckosher.org",  # Added email address
+        "email": "matthewjmiller07@gmail.com",  # Added email address
     },
     "rav_asher_weiss.xml": {
         "speaker_id": 860,  # TorahAnytime Speaker ID for Rav Asher Weiss
@@ -35,7 +35,7 @@ rss_feeds = {
         "title": "Rav Asher Weiss' Torah",
         "description": "Shiurim from Rav Asher Weiss, Shlit”a",
         "author": "Rav Asher Weiss",  # Added author key
-        "email": "info@minchasasher.com",  # Added email address
+        "email": "matthewjmiller07@gmail.com",  # Added email address
     },
     "shearim_btefillah.xml": {
         "search_query": "She'arim B'Tefillah",  # Search query for the new feed
@@ -44,7 +44,7 @@ rss_feeds = {
         "title": "She'arim B'Tefillah",  # Title for She'arim B'Tefillah
         "description": "Shiurim from She'arim B'Tefillah",
         "author": "She'arim B'Tefillah",  # Author for She'arim B'Tefillah
-        "email": "office@brsonline.org",  # Added email address
+        "email": "matthewjmiller07@gmail.com",  # Added email address
     },
 }
 
